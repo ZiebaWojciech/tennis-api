99d6eb6cad91986f22ce47c2a18edcbbb2d5d4b0
Wojciech Zięba <34630361+ZiebaWojciech@users.noreply.github.com>
2018-09-15 18:56:21 +0200

    Working swagger configuration

---
+++ src/main/java/pl/coderslab/feedingtennisapi/rest.java
@@ -0,0 +1,4 @@
+package pl.coderslab.feedingtennisapi;
+
+public class rest {
+}
---
+++ src/main/java/pl/coderslab/feedingtennisapi/rest/PlayerController.java
@@ -0,0 +1,10 @@
+package pl.coderslab.feedingtennisapi.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/players")
+public class PlayerController {
+    public String test(){}
+}
